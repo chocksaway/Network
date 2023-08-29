6d4dbba63a11693cd0e763c034731279f2a7a610
milesd <milesd@chocksaway.com>
2023-08-29 21:50:47 +0100

    Replaced with single import.

--- src/main/java/com/chocksaway/MulticastServer.java
+++ src/main/java/com/chocksaway/MulticastServer.java
@@ -1,7 +1,10 @@
 package com.chocksaway;
 
 import java.io.IOException;
-import java.net.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Date;
 
 public class MulticastServer {
